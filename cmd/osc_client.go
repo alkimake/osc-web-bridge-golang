0ef13b9c4bf33fc0dd708bb1831346dc96961d04
Alkim Ake Gozen <alkimake@pm.me>
2024-09-28 13:38:14 +0900

    osc_client: random messages on every 3 seconds

--- cmd/osc_client.go
+++ cmd/osc_client.go
@@ -1,12 +1,23 @@
 package main
 
-import "github.com/hypebeast/go-osc/osc"
+import (
+	"time"
+
+	"math/rand"
+
+	"github.com/hypebeast/go-osc/osc"
+)
 
 func main() {
 	client := osc.NewClient("239.255.0.1", 9000)
-	msg := osc.NewMessage("/osc/address")
-	msg.Append(int32(111))
-	msg.Append(true)
-	msg.Append("hello")
-	client.Send(msg)
+
+	for {
+		msg := osc.NewMessage("/osc/address")
+		msg.Append(int32(rand.Intn(100))) // Random integer
+		msg.Append(rand.Intn(2) == 1)     // Random boolean
+		msg.Append("hello")               // Static string or can be randomized
+
+		client.Send(msg)
+		time.Sleep(3 * time.Second) // Wait for 3 seconds
+	}
 }
